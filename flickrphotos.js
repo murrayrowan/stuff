1a3d75c7b1d47d9640a4208ac528fd86b8d4dcb1
Murray Rowan <murrayrowan@yahoo.co.uk>
2013-01-14 17:42:50 +0000

    add new

---
+++ flickrphotos.js
@@ -0,0 +1,133 @@
+{
+ "query": {
+  "count": 10,
+  "created": "2013-01-14T12:42:05Z",
+  "lang": "en-US",
+  "diagnostics": {
+   "publiclyCallable": "true",
+   "url": {
+    "execution-start-time": "1",
+    "execution-stop-time": "192",
+    "execution-time": "191",
+    "content": "http://api.flickr.com/services/rest/?method=flickr.photos.search&text=cat&page=1&per_page=10"
+   },
+   "user-time": "192",
+   "service-time": "191",
+   "build-version": "32943"
+  },
+  "results": {
+   "photo": [
+    {
+     "farm": "9",
+     "id": "8380481318",
+     "isfamily": "0",
+     "isfriend": "0",
+     "ispublic": "1",
+     "owner": "91535523@N02",
+     "secret": "aab220a860",
+     "server": "8335",
+     "title": "maine coon Florimon 08"
+    },
+    {
+     "farm": "9",
+     "id": "8380481178",
+     "isfamily": "0",
+     "isfriend": "0",
+     "ispublic": "1",
+     "owner": "91535523@N02",
+     "secret": "84deb2cc64",
+     "server": "8217",
+     "title": "maine coon Florimon 09"
+    },
+    {
+     "farm": "9",
+     "id": "8379395277",
+     "isfamily": "0",
+     "isfriend": "0",
+     "ispublic": "1",
+     "owner": "15009582@N08",
+     "secret": "0c15e2617d",
+     "server": "8513",
+     "title": "要撥開像這樣趴在腿上睡覺的貓咪有些些捨不得哩~ #cat"
+    },
+    {
+     "farm": "9",
+     "id": "8380471682",
+     "isfamily": "0",
+     "isfriend": "0",
+     "ispublic": "1",
+     "owner": "22279197@N00",
+     "secret": "005b3ee221",
+     "server": "8373",
+     "title": "tjasse3days_004"
+    },
+    {
+     "farm": "9",
+     "id": "8379395007",
+     "isfamily": "0",
+     "isfriend": "0",
+     "ispublic": "1",
+     "owner": "88131075@N00",
+     "secret": "d492a1aab8",
+     "server": "8084",
+     "title": "boxed"
+    },
+    {
+     "farm": "9",
+     "id": "8379381225",
+     "isfamily": "0",
+     "isfriend": "0",
+     "ispublic": "1",
+     "owner": "68224568@N08",
+     "secret": "0999395820",
+     "server": "8363",
+     "title": "Nice day"
+    },
+    {
+     "farm": "9",
+     "id": "8379384581",
+     "isfamily": "0",
+     "isfriend": "0",
+     "ispublic": "1",
+     "owner": "40297601@N02",
+     "secret": "21549f51c5",
+     "server": "8330",
+     "title": "Great Gray Owl...#6"
+    },
+    {
+     "farm": "9",
+     "id": "8379389331",
+     "isfamily": "0",
+     "isfriend": "0",
+     "ispublic": "1",
+     "owner": "73499595@N07",
+     "secret": "bac3842476",
+     "server": "8183",
+     "title": "出てこいー #ねこ #cat"
+    },
+    {
+     "farm": "9",
+     "id": "8380467028",
+     "isfamily": "0",
+     "isfriend": "0",
+     "ispublic": "1",
+     "owner": "88868678@N07",
+     "secret": "893f6d5a37",
+     "server": "8236",
+     "title": "Ariel"
+    },
+    {
+     "farm": "9",
+     "id": "8379394121",
+     "isfamily": "0",
+     "isfriend": "0",
+     "ispublic": "1",
+     "owner": "88131075@N00",
+     "secret": "8f6259f234",
+     "server": "8466",
+     "title": "outdorsy"
+    }
+   ]
+  }
+ }
+}
