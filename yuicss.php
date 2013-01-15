315c7eb10a2f837442f0c36607748291114f07b8
Murray Rowan <murrayrowan@yahoo.co.uk>
2013-01-15 09:32:56 +0000

    add new

---
+++ yuicss.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>YUI CSS in Test Document</title>
+
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.8.0/build/cssreset/cssreset-min.css">
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.8.0/build/cssgrids/grids-min.css">
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.8.0/build/cssbase/cssbase-min.css">
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.8.0/build/cssfonts/cssfonts-min.css">
+<link rel="stylesheet" type="text/css" href="main.css">
+
+</head>
+<body>
+
+	 <div class="yui3-g">
+        <div class="yui3-u-5-24">
+			<h2>navigation here</h2>
+        </div>
+        <div class="yui3-u-19-24">
+			<h1>Header 1</h1>
+        </div>
+    </div>
+
+
+
+
+<script src="http://yui.yahooapis.com/3.8.0/build/yui/yui-min.js"></script>
+<script>
+
+// Create a YUI sandbox on your page.
+YUI().use('node', 'yql', function(Y) {
+// yui seed	
+});
+</script>
+</body>
+</head>
