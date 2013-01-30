ab9dde24d6d41b4246cf81663eec18761dc832ca
Murray Rowan <murrayrowan@yahoo.co.uk>
2013-01-30 11:59:57 +0000

    Update insertion_sort.php

--- insertion_sort.php
+++ insertion_sort.php
@@ -2,15 +2,15 @@
 
 function insertion_sort($numbers){
 
-$count = count($numbers); // count array elements
+$count = count($numbers);    // count array elements
 
-for($i=1;$i<$count;$i++){ // $i is the index of the first unsorted element in the list
-  $j=$i-1; // $j is the index of the last element in the sorted part of the list
+for($i=1;$i<$count;$i++){    // $i is the index of the first unsorted element in the list
+  	$j=$i-1; 	     // $j is the index of the last element in the sorted part of the list
 	$key = $numbers[$i]; // $key is a temp holder for the first unsorted element. This is needed because we will later overwrite $numbers[$i]
-	while($j>=0 && $numbers[$j] > $key){ // while we're not at the start of the list AND WHILE one of the sorted elements is greater than the unsorted one
+	while($j>=0 && $numbers[$j] > $key){ 	 // while we're not at the start of the list AND WHILE one of the sorted elements is greater than the unsorted one
 		$numbers[$j+1] = $numbers[$j];   // swap the larger element for the next one down
-		$numbers[$j]= $key;  			 // make the current element equal to the temp value in $key to complete the swap
-		$j=$j-1;		     			 // move down to the next element in the list 
+		$numbers[$j]= $key;  		 // make the current element equal to the temp value in $key to complete the swap
+		$j=$j-1;		     	 // move down to the next element in the list 
 	}
 }
 	return $numbers; // return the sorted list
