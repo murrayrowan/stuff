fbb3d45e097abcd7840dd95e55c8c39cf406ebef
Murray Rowan <murrayrowan@yahoo.co.uk>
2013-01-31 15:10:11 +0000

    add comments

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,16 +1,16 @@
 
-def insertionsort(num)
-  msg = "[ Insertion Sort ]"
-	puts msg
+def insertionsort(num) ## define IS function
+	msg = "[ Insertion Sorted List ]"
+	puts msg ## construct and pring useful message to console
 
 i = 0
-until i == num.length - 1 do
-	i += 1 unless i == num.length
+until i == num.length - 1 do ## create loop to iterate through the array
+	i += 1 unless i == num.length 
 	j = i
 
-	while j >= 0 and num[j-1] > num[j]
-		num[j], num[j-1] = num[j-1], num[j]
-		j-=1 unless j == 1
+	while j >= 0 and num[j-1] > num[j] ## secondary loop to swap elements into the sorted array when they are smaller than sorted array elements
+		num[j], num[j-1] = num[j-1], num[j] ## cool swap method that doesn't require a temp variable
+		j-=1 unless j == 1 
 	end
 
 end
@@ -21,10 +21,10 @@ end
 
 numbers = [23,34,46,87,12,1,66]
 
-msg = "[ Unsorted ]"
+msg = "[ Unsorted List ]"
 puts msg
 puts numbers
 
-sorted = insertionsort(numbers)
+sorted = insertionsort(numbers) ## call the insertion sort method
 
-puts sorted
+puts sorted ## print the sorted list
